1683d470d2b52dc18bcdd430407527c5d1462737
ChelseaPanchal <72443485+ChelseaPanchal@users.noreply.github.com>
2022-06-01 15:11:21 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmdPC-MvUf3s5asBAMaQ-l1zNh1J4_01c",
+    authDomain: "storytellingapp-b8dfe.firebaseapp.com",
+    databaseURL: "https://storytellingapp-b8dfe-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-b8dfe",
+    storageBucket: "storytellingapp-b8dfe.appspot.com",
+    messagingSenderId: "55668156238",
+    appId: "1:55668156238:web:3a9d2340e407c959fce01f"
+  };
